d64178d9c5f99e19b39597a80c08ff3f3d5b645a
miiwo <chellewsa@gmail.com>
2022-12-02 17:51:02 -0500

    Complete part 1

--- day2/day2.rs
+++ day2/day2.rs
@@ -1,14 +1,70 @@
-use std::io
+use std::fs::File;
+use std::io::{BufRead, BufReader};
+use std::collections::HashMap;
 
 fn main() {
-    println!("Hello World!")
+    println!("The total score for part 1 is: {}", part1())
 }
 
 fn part1() -> u32 {
-    // read input line by line
-        // figure out what shape you selected for the round
-        // figure out who won the round
-        // add to a running total score
-    // return total score
+    let rock = HandShape {
+        key: String::from("X"),
+        opponent_key: String::from("A"),
+        shape_value: 1,
+        beats: String::from("C")
+    };
+
+    let paper = HandShape {
+        key: String::from("Y"),
+        opponent_key: String::from("B"),
+        shape_value: 2,
+        beats: String::from("A")
+    };
+
+    let scissors = HandShape {
+        key: String::from("Z"),
+        opponent_key: String::from("C"),
+        shape_value: 3,
+        beats: String::from("B")
+    };
+
+    let rock_paper_scissors_scorebook = HashMap::from([
+        (String::from("X"), rock),
+        (String::from("Y"), paper),
+        (String::from("Z"), scissors)
+    ]);
+
+    let mut total_score: u32 = 0;
+
+    let file = File::open("input").unwrap();
+    let reader = BufReader::new(file);
+
+    for (_index, line) in reader.lines().enumerate() {
+        let line = line.unwrap();
+        let player_shape: String = (&line[2..3]).to_string();
+        let opponent_shape: String = (&line[0..1]).to_string();
+
+        let shape_score: u32 = rock_paper_scissors_scorebook[&player_shape].shape_value.into();
+        let round_score: u32 = round_score(&rock_paper_scissors_scorebook[&player_shape], &opponent_shape);
+        total_score = total_score + shape_score + round_score;
+    }
+
+    total_score
+}
+
+fn round_score(player: &HandShape, opponent: &String) -> u32 {
+    println!("Player: {}, Opponent: {}", player.opponent_key, opponent);
+    match player {
+        HandShape { beats, .. } if beats == opponent => 6,
+        HandShape { opponent_key, .. } if opponent_key == opponent => 3,
+        _ => 0
+    }
+}
+
+struct HandShape {
+    key: String,
+    opponent_key: String,
+    shape_value: u8,
+    beats: String
 }
 
