a6138fb048924eba5afedabbd85b167081e8fa47
miiwo <chellewsa@gmail.com>
2022-12-02 13:11:29 -0500

    Base code

---
+++ day2/day2.rs
@@ -0,0 +1,14 @@
+use std::io
+
+fn main() {
+    println!("Hello World!")
+}
+
+fn part1() -> u32 {
+    // read input line by line
+        // figure out what shape you selected for the round
+        // figure out who won the round
+        // add to a running total score
+    // return total score
+}
+
