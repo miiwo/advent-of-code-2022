017e9cfc8950fad6d75828af4f9df9f2f6c8828c
miiwo <chellewsa@gmail.com>
2022-12-03 00:18:27 -0500

    Write pseudocode

---
+++ day3/day3.py
@@ -0,0 +1,9 @@
+# setup hashmap of boolean
+
+# read file line by line
+# split line in half
+# go through first half and turn hash map on ; )
+# go through second half checking if any keys are turned on
+# if key turned on, its a duplicate, return priority
+# sum up each line/rucksack
+# return priority sum
