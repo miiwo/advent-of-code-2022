d181a90e08bd586e10c089878ee1b48f3ebfa51f
miiwo <chellewsa@gmail.com>
2022-12-04 00:52:31 -0500

    I think i added unnecessary code, but i get MY answer instead of another

--- day4/day4.py
+++ day4/day4.py
@@ -11,4 +11,23 @@ def part1() -> int:
 
     return result
 
+def part2() -> int:
+    result = 0
+    with open("./input", "r") as elf_file:
+        for line in elf_file:
+            assignedSections = line.split(",")
+            elfOne = assignedSections[0].split("-")
+            elfTwo = assignedSections[1].split("-")
+
+            # fix which elf has the higher last assignment scope
+            if elfOne[1] < elfTwo[1]:
+                temp = elfTwo
+                elfTwo = elfOne
+                elfOne = temp
+
+            if (int(elfOne[1]) >= int(elfTwo[0]) and int(elfOne[0]) <= int(elfTwo[0])) or (int(elfTwo[1]) >= int(elfOne[0]) and int(elfTwo[0]) <= int(elfOne[0])):
+                result += 1
+    return result
+
 print(part1())
+print(part2())
