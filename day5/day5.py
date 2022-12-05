3615bd790657f0265f3cdf1bb0e226b6410d1cc4
miiwo <chellewsa@gmail.com>
2022-12-05 01:09:09 -0500

    Complete part 1

--- day5/day5.py
+++ day5/day5.py
@@ -1,20 +1,35 @@
 from collections import deque
+import re
 
 def part1() -> str:
     result = ""
     # make list of stacks
     supplyStacks = []
+    doneInit = False
 
     with open("./input", "r") as elf_file:
         for line in elf_file:
-            # for gods heaven, parse into a list of stacks
+            if line == "\n":
+                doneInit = True
+                continue
+            if not doneInit:
+                piles = re.findall("\D{3}\s|\n", line)
+                for (index, pile) in enumerate(piles):
+                    if len(supplyStacks) == 0:
+                        supplyStacks = [deque() for x in range(len(piles))]
+                    
+                    if len(pile.strip()) != 0:
+                        supplyStacks[index].appendleft(pile)
+                continue
             # for somewhat bleh, parse the movement pattern
-            sourceStack = 0
-            targetStack = 0
+            moveCommands = re.findall("\d+", line)
+            sourceStack = int(moveCommands[1]) - 1
+            targetStack = int(moveCommands[2]) - 1
             # move the items accordingly in stack
-            supplyStacks[targetStack].append(supplyStacks[sourceStack].pop())
+            for i in range(0, min(int(moveCommands[0]), len(supplyStacks[sourceStack]))):
+                supplyStacks[targetStack].append(supplyStacks[sourceStack].pop())
         # pop an element off each stack and return as result
-    return result
+    return list(map(lambda crate: crate.pop(), supplyStacks))
 
 def part2() -> int:
     result = 0
